68308c9e561d54929f37a42e73d0a98dec50544f
minhnhat0809 <137554321+minhnhat0809@users.noreply.github.com>
2025-02-18 12:38:13 +0700

    ContentService: Config azure

--- ContentService.Api/Controllers/ReactionController.cs
+++ ContentService.Api/Controllers/ReactionController.cs
@@ -1,6 +1,7 @@
 using ContentService.Application.Commands;
 using ContentService.Application.DTOs.ReactionDtos;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContentService.Api.Controllers
@@ -12,9 +13,14 @@ namespace ContentService.Api.Controllers
         private readonly IMediator _mediator = mediator;
 
         [HttpPost]
+        [Authorize]
         public async Task<IActionResult> CreateReaction( [FromBody] ReactionCreateDto reactionCreateDto)
         {
-            var result = await _mediator.Send(new CreateReactionCommand(reactionCreateDto));
+            var value = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "custom_id")?.Value;
+            if (value == null) return StatusCode(StatusCodes.Status401Unauthorized);
+            var userBrowsingId = int.Parse(value);
+            
+            var result = await _mediator.Send(new CreateReactionCommand(userBrowsingId, reactionCreateDto));
             
             return StatusCode(result.StatusCode, result);
         }
--- ContentService.Application/Commands/CreateReplyCommand.cs
+++ ContentService.Application/Commands/CreateReplyCommand.cs
@@ -5,9 +5,9 @@ using Shared.Dtos;
 
 namespace ContentService.Application.Commands;
 
-public class CreateReplyCommand(ReplyCreateDto replyCreateDto, List<IFormFile> files) : IRequest<ResponseDto>
+public class CreateReplyCommand(int creatorId, ReplyCreateDto replyCreateDto, List<IFormFile> files) : IRequest<ResponseDto>
 {
-    public int CreatorId { get; set; } = replyCreateDto.CreatorId;
+    public int CreatorId { get; set; } = creatorId;
     
     public int CommentId { get; set; } = replyCreateDto.CommentId;
     
