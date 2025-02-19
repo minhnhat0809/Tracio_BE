dae3d086439e0fb48f83ef3dd4c43d879f0fc874
minhnhat0809 <137554321+minhnhat0809@users.noreply.github.com>
2025-02-19 21:45:05 +0700

    ContentService: Get blog categories

---
+++ ContentService.Application/DTOs/BlogDtos/ViewDtos/BlogCategoryDtos.cs
@@ -0,0 +1,8 @@
+namespace ContentService.Application.DTOs.BlogDtos.ViewDtos;
+
+public class BlogCategoryDtos
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+}
